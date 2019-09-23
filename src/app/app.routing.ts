2eec686489a890ad61b1ad399e8b5b47d641d970
albertoliu1993 <albertoliu1993@gmail.com>
2019-09-23 18:43:20 -0400

    Merged with User page version

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,15 +1,16 @@
 ﻿import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-
-
-
+import { ResourcePageComponent } from './resource-page/resource-page.component';
+import { HeaderComponent } from './header/header.component';
 
 
 const appRoutes: Routes = [
     {path: '', component: LoginComponent},
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
+    { path: 'resourcepage', component: ResourcePageComponent},
+    {path: 'header', component: HeaderComponent },
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
